08c8cd217301837b494b17589c276b5658c4b8f1
Dagan Martinez <dmartinez@starry.com>
2022-03-20 15:13:50 -0400

    Document value module

--- jabroni/src/value.rs
+++ jabroni/src/value.rs
@@ -12,19 +12,40 @@ use std::{
 type Number = i32;
 
 type SubroutineCallback = Box<dyn Fn(BindingMap, &mut [Value]) -> JabroniResult<Value>>;
+
 #[derive(Clone)]
+/// A Jabroni function.
+/// # Example
+/// ```
+/// use jabroni::{Binding, BindingMap, Jabroni, Subroutine, Value as JabroniValue};
+///
+/// let mut interpreter = Jabroni::new();
+/// interpreter.define_constant(
+///     "add_one",
+///     JabroniValue::Subroutine(Subroutine::new(
+///         1,
+///         Box::new(|_context: BindingMap, args: &mut [JabroniValue]| {
+///             Ok(JabroniValue::Number(*args[0].as_number().unwrap() + 1))
+///         }),
+///     )),
+/// );
+/// assert_eq!(interpreter.run_expression("add_one(1)").unwrap(), 2.into());
+/// ```
 pub struct Subroutine {
     number_of_args: Option<usize>,
     callback: Rc<SubroutineCallback>,
 }
 
 impl Subroutine {
+    /// Construct a new Jabroni function.
     pub fn new(number_of_args: usize, callback: SubroutineCallback) -> Self {
         Self {
             number_of_args: Some(number_of_args),
             callback: Rc::new(callback),
         }
     }
+
+    /// Construct a new Jabroni function that can take any number of arguments.
     pub fn new_variadic(callback: SubroutineCallback) -> Self {
         Self {
             number_of_args: None,
@@ -32,6 +53,7 @@ impl Subroutine {
         }
     }
 
+    /// Call the function.
     pub fn call(&self, context: BindingMap, args: &mut [Value]) -> JabroniResult<Value> {
         if let Some(number_of_args) = self.number_of_args {
             if args.len() != number_of_args {
@@ -57,20 +79,43 @@ impl PartialEq for Subroutine {
 }
 
 #[derive(PartialEq, Debug, Clone, EnumAsInner)]
+/// Enumeration of the different types in Jabroni.
 pub enum Value {
+    /// Number type
     Number(Number),
+    /// Boolean type
     Boolean(bool),
+    /// String type
     String(String),
+    /// Object type
     Object(BindingMap),
+    /// Function type
     Subroutine(Subroutine),
+    /// Null type - corresponds to Javascript's Null/Undefined
     Null,
 }
 
 impl Value {
+    /// Create a String value form a quoted string literal.
+    ///
+    /// #Example
+    /// ```
+    /// use jabroni::Value as JabroniValue;
+    /// let value = JabroniValue::from_string_literal("\"hello\"").unwrap();
+    /// assert_eq!(value, JabroniValue::String("hello".into()));
+    /// ```
     pub fn from_string_literal(literal: &str) -> JabroniResult<Self> {
         Ok(Value::String(utils::unquote(literal)?))
     }
 
+    /// Construct a new Number value from a numeric literal.
+    ///
+    /// #Example
+    /// ```
+    /// use jabroni::Value as JabroniValue;
+    /// let value = JabroniValue::from_numeric_literal("42").unwrap();
+    /// assert_eq!(value, JabroniValue::Number(42.into()));
+    /// ```
     pub fn from_numeric_literal(literal: &str) -> JabroniResult<Self> {
         Ok(Value::Number(
             literal
@@ -80,6 +125,14 @@ impl Value {
         ))
     }
 
+    /// Construct a new Boolean value from a boolean literal.
+    ///
+    /// #Example
+    /// ```
+    /// use jabroni::Value as JabroniValue;
+    /// let value = JabroniValue::from_boolean_literal("true").unwrap();
+    /// assert_eq!(value, JabroniValue::Boolean(true.into()));
+    /// ```
     pub fn from_boolean_literal(literal: &str) -> JabroniResult<Self> {
         if literal == "true" {
             Ok(Value::Boolean(true))
@@ -107,21 +160,25 @@ impl Value {
         }
     }
 
+    /// Add a Number value
     pub fn add(&mut self, value: Value) -> JabroniResult {
         *self.unwrap_as_number()? += value.unwrap_into_number()?;
         Ok(())
     }
 
+    /// Subtract a Number value
     pub fn subtract(&mut self, value: Value) -> JabroniResult {
         *self.unwrap_as_number()? -= value.unwrap_into_number()?;
         Ok(())
     }
 
+    /// Multiply with a Number value
     pub fn multiply(&mut self, value: Value) -> JabroniResult {
         *self.unwrap_as_number()? *= value.unwrap_into_number()?;
         Ok(())
     }
 
+    /// Negate the value (bools only)
     pub fn inverse(&mut self) -> JabroniResult {
         match self {
             Self::Boolean(boolean) => *self = Self::Boolean(!*boolean),
@@ -132,6 +189,8 @@ impl Value {
         Ok(())
     }
 
+    /// Compare equality. `allow_type_diff` allows for comparisons between different types (always
+    /// false)
     pub fn compare(&mut self, value: Value, allow_type_diff: bool) -> JabroniResult {
         if std::mem::discriminant(self) != std::mem::discriminant(&value) {
             *self = false.into();
@@ -166,6 +225,7 @@ impl Value {
         Ok(())
     }
 
+    /// Compare with custom comparator. Type differences are not allowed
     pub fn compare_inequality(
         &mut self,
         value: Value,
