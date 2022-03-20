8e9853f16182ac6740b4d528aa4f2f7d21084011
Dagan Martinez <dmartinez@starry.com>
2022-03-19 20:36:18 -0400

    Allow Jabroni CLI to run scripts

--- jabroni-cli/src/main.rs
+++ jabroni-cli/src/main.rs
@@ -1,19 +1,35 @@
+use anyhow::Result;
 use jabroni::Jabroni;
+use std::fs;
 use std::io::{self, Write};
-fn main() {
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "jabroni", about = "Jabroni interpreter")]
+struct Opt {
+    file: Option<PathBuf>,
+}
+
+fn main() -> Result<()> {
+    let opt = Opt::from_args();
     let mut jabroni = Jabroni::new();
+    if let Some(file) = opt.file {
+        jabroni.run_script(&fs::read_to_string(file)?)?;
+    } else {
+        loop {
+            print!("Jabroni> ");
+            io::stdout().flush().unwrap();
 
-    loop {
-        print!("Jabroni> ");
-        io::stdout().flush().unwrap();
-
-        let mut line = String::new();
-        io::stdin()
-            .read_line(&mut line)
-            .expect("Failed to read line");
-        match jabroni.run_expression(line.trim()) {
-            Ok(value) => println!("{}", value),
-            Err(e) => println!("{}", e),
-        };
+            let mut line = String::new();
+            io::stdin()
+                .read_line(&mut line)
+                .expect("Failed to read line");
+            match jabroni.run_expression(line.trim()) {
+                Ok(value) => println!("{}", value),
+                Err(e) => println!("{}", e),
+            };
+        }
     }
+    Ok(())
 }
